796f3f04308a1ce0a048bff8429390ef7b4e5e70
mukulsomukesh <mukulsomukesh@gmail.com>
2025-01-12 05:04:09 +0530

    feat: add user profile retrieval and update functionality with Cloudinary integration

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const { registerUser, loginUser, forgotPassword, setNewPassword } = require('../controllers/user.controller');
+const { registerUser, loginUser, forgotPassword, setNewPassword, getUserProfile, updateUserProfile } = require('../controllers/user.controller');
+const authMiddleware = require('../middleware/auth.middleware');
 
 
 const router = express.Router();
@@ -9,5 +10,9 @@ router.route('/signup').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/forgot-password').post(forgotPassword);
 router.route('/update-password').post(setNewPassword);
+
+router.route('/user-profile').get(authMiddleware, getUserProfile);
+router.route('/update-user-profile').put(authMiddleware, updateUserProfile);
+
 
 module.exports = router
