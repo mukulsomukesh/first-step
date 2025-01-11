3d6700bfc525c8c10ed12424371bf27afd2ffe4b
mukulsomukesh <mukulsomukesh@gmail.com>
2025-01-12 04:54:10 +0530

    feat: enhance profile dropdown with user image and name

--- client/src/app/components/commonComponents/ProfileDropdown.jsx
+++ client/src/app/components/commonComponents/ProfileDropdown.jsx
@@ -1,13 +1,23 @@
 "use client"
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { toast } from "react-toastify";
 
 const ProfileDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [userDetails, setUserDetails] = useState(null);
   const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUserDetails = localStorage.getItem("notesMasterUserDetails");
+      if (storedUserDetails) {
+        setUserDetails(JSON.parse(storedUserDetails));
+      }
+    }
+  }, []);
 
   // handle logout
   const handleLogout = () => {
@@ -23,13 +33,40 @@ const ProfileDropdown = () => {
     router.push(path); // Navigate to the passed path
   };
 
+  // Close the dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isOpen && !event.target.closest(".profile-dropdown")) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className="relative text-primary-950">
+    <div className="relative text-primary-950 profile-dropdown">
       <button
         className="flex items-center space-x-2"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <FaUserCircle className="text-primary-950 text-4xl" />
+        {userDetails?.name && (
+          <strong className="text-primary-600 text-[20px] ">
+            Hi, {userDetails.name.split(" ")[0].charAt(0).toUpperCase() + userDetails.name.split(" ")[0].slice(1)}
+          </strong>
+        )}
+        {userDetails?.profilePicture ? (
+          <img
+            src={userDetails.profilePicture}
+            alt="User Profile"
+            className="w-10 h-10 rounded-full border-2 border-primary-600"
+          />
+        ) : (
+          <FaUserCircle className="text-primary-950 text-4xl" />
+        )}
       </button>
       {isOpen && (
         <ul className="absolute min-w-[200px] right-0 mt-2 bg-primary-50 shadow-xl border-2 border-primary-600 border-t rounded-md overflow-hidden">
--- client/src/app/services/auth.js
+++ client/src/app/services/auth.js
@@ -5,6 +5,7 @@ export const loginService = (payload) => {
     try {
       let response = await axiosInstance.post(`/api/auth/login`, payload);
       localStorage.setItem("token", response.jwtToken); // Corrected method
+      localStorage.setItem("notesMasterUserDetails", JSON.stringify(response.Details)); // Corrected method
       resolve(response);
     } catch (error) {
       reject({ message: error });
@@ -40,6 +41,29 @@ export const resetPasswordService  = (payload) => {
   return new Promise(async (resolve, reject) => {
     try {
       let response = await axiosInstance.post(`/api/auth/update-password`, payload);
+      resolve(response);
+    } catch (error) {
+      reject({ message: error });
+    }
+  });
+};
+
+export const getUserProfileService  = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = await axiosInstance.get(`/api/auth/user-profile`);
+      resolve(response);
+    } catch (error) {
+      reject({ message: error });
+    }
+  });
+};
+
+
+export const updateUserProfileService  = (formData) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let response = await axiosInstance.put(`/api/auth/update-user-profile`,formData );
       resolve(response);
     } catch (error) {
       reject({ message: error });
